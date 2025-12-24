98db324cbdf817f2d4c44ecb4b230c461464b5bb
Klim Trotsenko <klim.trotsenko7@gmail.com>
2025-12-24 15:18:02 +0700

    deleted console.log

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,9 @@
 function isStringLengthValid(text, maxLimit){
   return text.length <= maxLimit;
 }
-// Строка короче 20 символов
-console.log(isStringLengthValid('проверяемая строка', 20)); // true
-// Длина строки ровно 18 символов
-console.log(isStringLengthValid('проверяемая строка', 18)); // true
-// Строка длиннее 10 символов
-console.log(isStringLengthValid('проверяемая строка', 10)); // false
-
+isStringLengthValid('проверяемая строка', 20);
+isStringLengthValid('проверяемая строка', 18);
+isStringLengthValid('проверяемая строка', 10);
 
 function isPalindromValid(text) {
   const cleanedText = text.toLowerCase().replaceAll(' ', '');
@@ -20,16 +16,9 @@ function isPalindromValid(text) {
 
   return (newText === cleanedText);
 }
-
-// Строка является палиндромом
-console.log(isPalindromValid('топот')); // true
-// Несмотря на разный регистр, тоже палиндром
-console.log(isPalindromValid('ДовОд')); // true
-// Это не палиндром
-console.log(isPalindromValid('Кекс')); // false
-
-
-//Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN:
+isPalindromValid('топот');
+isPalindromValid('ДовОд');
+isPalindromValid('Кекс');
 
 function getNumber (text) {
   text = text.toString();
@@ -47,11 +36,11 @@ function getNumber (text) {
   return Number(count);
 }
 
-console.log(getNumber('2023 год'));// 2023
-console.log(getNumber('ECMAScript 2022'));// 2022
-console.log(getNumber('1 кефир, 0.5 батона')); // 105
-console.log(getNumber('агент 007'));// 7
-console.log(getNumber('а я томат'));// NaN
-console.log(getNumber((2023)));// 2023
-console.log(getNumber((-1)));// 1
-console.log(getNumber((1.5)));// 15
+getNumber('2023 год');
+getNumber('ECMAScript 2022');
+getNumber('1 кефир, 0.5 батона');
+getNumber('агент 007');
+getNumber('а я томат');
+getNumber((2023));
+getNumber((-1));
+getNumber((1.5));
